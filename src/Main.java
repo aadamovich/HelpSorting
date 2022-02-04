4d36a8451fa24f52140f2c20d65ca52fb3c60dd7
Monta Kaļva <montamona@inbox.lv>
2022-02-04 20:52:49 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Declare variable
+        byte num1;
+        // Initialization
+        num1 = 10;
+        // Print out variables value in console
+        System.out.println(num1);
+
+        // Declaration and initialization at the same line
+        String name = "Learn";
+        System.out.println(name);
+
+        short num2 = 2000;
+        int num3 = 1226845774;
+        long num4 = 145252425678757L;
+
+        System.out.println( num2 + '\n' + num3);
+
+
+
+    }
+}
