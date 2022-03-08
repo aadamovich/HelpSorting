d39e6cc8fd6139501736e1c5e4dcacc79aedc43c
Monta Kaļva <montamona@inbox.lv>
2022-03-08 21:52:32 +0200

    Extra methods to Homework_9 Methods

--- src/homeworks/Homework_9_Methods.java
+++ src/homeworks/Homework_9_Methods.java
@@ -6,10 +6,17 @@ public class Homework_9_Methods {
     public static void main(String[] args) {
 
         int[] array = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49};
+        int[] array2 = {12, 65, 98, 87, 23, 56, 1, 44, 8, 100};
         System.out.println("Original array: " + Arrays.toString(array));
 
-        System.out.println("Maximum value for the above array: " + max(array));
         System.out.println("Minimum value for the above array: " + min(array));
+        System.out.println("Minimum value for the above array: " + min(array2));
+        System.out.println("Maximum value for the above array: " + max(array));
+        System.out.println("Maximum value for the above array: " + max(array2));
+        System.out.println("Average value for the above array: " + average(array));
+        System.out.println("Average value for the above array: " + average(array2));
+        System.out.println("Sum value for the above array: " + sum(array));
+        System.out.println("Sum value for the above array: " + sum(array2));
     }
 
     public static int max(int[] array) {
@@ -31,5 +38,24 @@ public class Homework_9_Methods {
         }
         return minResult;
     }
+
+    public static int sum(int[] array){
+        int sum = 0;
+        for( int value : array){
+            sum += value;
+        }
+        return sum;
+    }
+
+    public static int average(int[] array){
+        int sum = 0;
+        int average = 0;
+        for (int value : array){
+            sum += value;
+            average = sum / array.length;
+        }
+        return average;
+    }
+
 }
 
