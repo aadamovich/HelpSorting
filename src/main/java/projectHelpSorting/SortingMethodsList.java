3b43c0ac23d3936fc1616e0d54c38c6fa2478afb
Andrey Adamovich <andrey@aestasit.com>
2022-05-30 12:14:36 +0300

    removed unused throws

--- src/main/java/projectHelpSorting/SortingMethodsList.java
+++ src/main/java/projectHelpSorting/SortingMethodsList.java
@@ -8,7 +8,7 @@ import java.util.Set;
 
 public class SortingMethodsList {
 
-    public static void donateMoney() throws Exception {
+    public static void donateMoney() {
         Scanner scanner = new Scanner(System.in);
         double moneyDonation;
 
